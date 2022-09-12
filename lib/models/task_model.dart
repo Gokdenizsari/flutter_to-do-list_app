a8e18d8269d419382f26a44a73e8ca660e33d438
Gökdeniz SARİİBRAHİM <gkdnzsrbrhm@gmail.com>
2022-09-12 12:34:00 +0300

    Create task_model.dart

---
+++ lib/models/task_model.dart
@@ -0,0 +1,10 @@
+class Task {
+  final String id;
+  final String name;
+  final DateTime createdAt;
+  final bool isCompleted;
+
+  Task({required this.id,required this.name,required this.createdAt,required this.isCompleted});
+
+  
+}
