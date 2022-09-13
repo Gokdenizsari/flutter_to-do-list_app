2caacb4e798f7bb78f1ef6e1fab3ff440446b4a1
Gökdeniz SARİİBRAHİM <gkdnzsrbrhm@gmail.com>
2022-09-13 16:22:59 +0300

    Update main_page.dart

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -3,37 +3,81 @@ import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_to_do_list_app/models/task_model.dart';
 
-class MainPage extends StatelessWidget {
+class MainPage extends StatefulWidget {
   const MainPage({Key? key}) : super(key: key);
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  late List<Task> _allTaks;
+  @override
+  void initState() {
+    super.initState();
+    _allTaks = <Task>[];
+    _allTaks.add(Task.create(name: "Try Task", createdAt: DateTime.now()));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: GestureDetector(
-          onTap: () {
-            _showAddTaskBottom(context);
-          },
-          child: Text(
-            "What will you do today?",
-            style: TextStyle(color: Colors.black),
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          title: GestureDetector(
+            onTap: () {
+              _showAddTaskBottom(context);
+            },
+            child: Text(
+              "What will you do today?",
+              style: TextStyle(color: Colors.black),
+            ),
           ),
+          centerTitle: false,
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.search),
+            ),
+            IconButton(
+                onPressed: () {
+                  _showAddTaskBottom(context);
+                },
+                icon: Icon(Icons.add)),
+          ],
         ),
-        centerTitle: false,
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.search),
-          ),
-          IconButton(
-              onPressed: () {
-                _showAddTaskBottom(context);
-              },
-              icon: Icon(Icons.add)),
-        ],
-      ),
-    );
+        body: _allTaks.isNotEmpty
+            ? ListView.builder(
+                itemBuilder: (context, index) {
+                  var _listEleman = _allTaks[index];
+                  return Dismissible(
+                    background: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.delete,
+                          color: Colors.red,
+                        ),
+                        SizedBox(width: 6),
+                        Text("Task Deleted"),
+                      ],
+                    ),
+                    key: Key(_listEleman.id),
+                    onDismissed: (direction) {
+                      _allTaks.remove(index);
+                      setState(() {});
+                    },
+                    child: ListTile(
+                      title: Text(_listEleman.name + " " + _listEleman.id),
+                      subtitle: Text(_listEleman.createdAt.toString()),
+                    ),
+                  );
+                },
+                itemCount: _allTaks.length,
+              )
+            : Center(
+                child: Text("Add to do Task"),
+              ));
   }
 
   void _showAddTaskBottom(BuildContext context) {
@@ -57,6 +101,8 @@ class MainPage extends StatelessWidget {
                         onConfirm: (time) {
                       var newAddToBeTask =
                           Task.create(name: value, createdAt: time);
+                      _allTaks.add(newAddToBeTask);
+                      setState(() {});
                     });
                   }
                 },
