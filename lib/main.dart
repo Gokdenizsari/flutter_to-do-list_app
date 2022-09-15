be190947100f0792293bcd4b83fbbf052ffcd20a
Gökdeniz SARİİBRAHİM <gkdnzsrbrhm@gmail.com>
2022-09-15 15:53:19 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_to_do_list_app/models/task_model.dart';
 import 'package:flutter_to_do_list_app/pages/main_page.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setSystemUIOverlayStyle(
       SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+
+  await Hive.initFlutter();
+  Hive.registerAdapter(TaskAdapter());
   runApp(const MyApp());
 }
 
