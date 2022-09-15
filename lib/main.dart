9f4310fd35334c2a07a8cb181bbafd0b1726f27e
Gökdeniz SARİİBRAHİM <gkdnzsrbrhm@gmail.com>
2022-09-15 16:19:25 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_to_do_list_app/data/local_storage.dart';
 import 'package:flutter_to_do_list_app/models/task_model.dart';
 import 'package:flutter_to_do_list_app/pages/main_page.dart';
+import 'package:get_it/get_it.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+
+final locator = GetIt.instance;
+
+void setup() {
+  locator.registerSingleton<LocalStorage>(HiveLocalStroge());
+}
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,6 +19,7 @@ Future<void> main() async {
 
   await Hive.initFlutter();
   Hive.registerAdapter(TaskAdapter());
+  setup();
   runApp(const MyApp());
 }
 
