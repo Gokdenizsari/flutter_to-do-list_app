d2ba33aea549fb8dfadad6b6dab32d30388d84e5
Gökdeniz SARİİBRAHİM <gkdnzsrbrhm@gmail.com>
2022-09-15 16:19:27 +0300

    Update local_storage.dart

--- lib/data/local_storage.dart
+++ lib/data/local_storage.dart
@@ -7,3 +7,37 @@ abstract class LocalStorage {
   Future<bool> deleteTask({required Task task});
   Future<Task> updateTask({required Task task});
 }
+
+ class HiveLocalStroge extends LocalStorage {
+  @override
+  Future<void> addTask({required Task task}) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<bool> deleteTask({required Task task}) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<Task>> getAllTask() {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Task> getTask({required String id}) {
+    throw UnimplementedError();
+  }
+  
+  @override
+  Future<void> addTasl({required Task task}) {
+    
+    throw UnimplementedError();
+  }
+  
+  @override
+  Future<Task> updateTask({required Task task}) {
+    
+    throw UnimplementedError();
+  }
+}
