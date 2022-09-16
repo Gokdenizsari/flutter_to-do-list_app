cb628f410fe1de7b15dee8c7928bb2704a50277e
Gökdeniz SARİİBRAHİM <gkdnzsrbrhm@gmail.com>
2022-09-16 15:50:02 +0300

    Update local_storage.dart

--- lib/data/local_storage.dart
+++ lib/data/local_storage.dart
@@ -1,14 +1,15 @@
 import 'package:flutter_to_do_list_app/models/task_model.dart';
+import 'package:hive/hive.dart';
 
 abstract class LocalStorage {
   Future<void> addTasl({required Task task});
-  Future<Task> getTask({required String id});
+  Future<Task?> getTask({required String id});
   Future<List<Task>> getAllTask();
   Future<bool> deleteTask({required Task task});
   Future<Task> updateTask({required Task task});
 }
 
-class MockLocalStorage extends LocalStorage{
+class MockLocalStorage extends LocalStorage {
   @override
   Future<void> addTasl({required Task task}) {
     // TODO: implement addTasl
@@ -40,37 +41,46 @@ class MockLocalStorage extends LocalStorage{
   }
 }
 
+class HiveLocalStroge extends LocalStorage {
+  late Box<Task> _taskbox;
 
- class HiveLocalStroge extends LocalStorage {
-  @override
-  Future<void> addTask({required Task task}) {
-    throw UnimplementedError();
+  HiveLocalStroge() {
+    _taskbox = Hive.box<Task>("Tasks");
   }
 
   @override
-  Future<bool> deleteTask({required Task task}) {
-    throw UnimplementedError();
+  Future<void> addTask({required Task task}) async {
+    await _taskbox.put(task.id, task);
   }
 
   @override
-  Future<List<Task>> getAllTask() {
-    throw UnimplementedError();
+  Future<bool> deleteTask({required Task task}) async {
+    await task.delete();
+    return true;
   }
 
   @override
-  Future<Task> getTask({required String id}) {
+  Future<List<Task>> getAllTask()async{
+    List<Task> _allTask = <Task>[];
+    _allTask = _taskbox.values.toList();
+    if (_allTask.length > 0) {
+      _allTask.sort((Task a, Task b) => a.createdAt.compareTo(b.createdAt));
+    }
+    return _allTask;
+  }
+
+  @override
+  Future<Task?> getTask({required String id}) {
     throw UnimplementedError();
   }
-  
+
   @override
   Future<void> addTasl({required Task task}) {
-    
     throw UnimplementedError();
   }
-  
+
   @override
   Future<Task> updateTask({required Task task}) {
-    
     throw UnimplementedError();
   }
 }
